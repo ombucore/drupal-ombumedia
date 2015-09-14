51cc1211c0f46ebfcdb76a8f23cefadd90b5e45f
Jon Duell <jon@jonduell.com>
2015-09-14 10:39:28 -0700

    Adds custom ombumedia field for handling view modes

--- js/ombumedia-field.js
+++ js/ombumedia-field.js
@@ -5,22 +5,24 @@
       $('.ombumedia-widget', context).each(function() {
         var $container = $(this);
         var $fid = $('input.fid', $container);
+        var $view_mode = $('input.view_mode', $container);
+
         var options = {
-          types: $container.attr('data-types'),
-          view_modes: $container.attr('data-view-modes')
+          types: $container.data('types'),
+          view_modes: $container.data('view-modes').split(',')
         };
 
         $('.select-media').on('click', function(e) {
           e.preventDefault();
 
-          if ($fid.val()) {
+          if ($fid.val() != 0) {
             options.fid = $fid.val();
           }
-          console.debug(options);
 
-          Drupal.ombumedia.selectMedia({
-          }).then(function(values) {
+          Drupal.ombumedia.selectMedia(options).then(function(values) {
             $fid.val(values.fid);
+            $view_mode.val(values.view_mode);
+
             $.get(Drupal.settings.basePath + 'file/' + values.fid + '/preview', function(content) {
               $('.preview', $container).html($('.ombumedia-file-preview .file-preview', content).html());
             });
